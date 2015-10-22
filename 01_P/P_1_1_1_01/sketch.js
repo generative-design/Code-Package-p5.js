96a7bd28b632c5367e86d2bb052f990da95c504d
Benedikt Groß <benedikt@looksgood.de>
2015-10-22 11:19:06 +0200

    translated P_1_1_1_01

---
+++ 01_P/P_1_1_1_01/sketch.js
@@ -0,0 +1,53 @@
+// P_1_1_1_01.pde
+// 
+// Generative Gestaltung, ISBN: 978-3-87439-759-9
+// First Edition, Hermann Schmidt, Mainz, 2009
+// Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+// Copyright 2009 Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * draw the color spectrum by moving the mouse
+ * 	 
+ * MOUSE
+ * position x/y        : resolution
+ * 
+ * KEYS
+ * s                   : save png
+ */
+ 
+var stepX;
+var stepY;
+
+function setup() {
+  createCanvas(800, 400);
+  background(0);
+}
+
+function draw() {
+  noStroke();
+  colorMode(HSB, width, height, 100);
+
+  stepX = mouseX+2;
+  stepY = mouseY+2;
+  for (var gridY=0; gridY<height; gridY+=stepY){
+    for (var gridX=0; gridX<width; gridX+=stepX){
+      fill(gridX, height-gridY, 100);
+      rect(gridX, gridY, stepX, stepY);
+    }
+  }
+}
+
+function keyPressed() {
+  if (key=='s' || key=='S') saveCanvas(gd.timestamp(), 'png');
+}
