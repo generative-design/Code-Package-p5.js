39616229be15f31314ad54905f12d60acaa9633f
Niels NTG <NTG999@gmail.com>
2016-01-31 21:33:19 +0100

    Added P_4_0_01

---
+++ 01_P/P_4_0_01/sketch.js
@@ -0,0 +1,50 @@
+// P_4_0_01.pde
+//
+// Generative Gestaltung, ISBN: 978-3-87439-759-9
+// First Edition, Hermann Schmidt, Mainz, 2009
+// Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+// Copyright 2009 Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * draw a grid of streched copies of an image
+ *
+ * MOUSE
+ * position x           : tile count horizontally
+ * position y           : tile count vertically
+ *
+ * KEYS
+ * s                    : save png
+ */
+
+var img;
+
+function setup() {
+  createCanvas(650,450);
+  img = loadImage("data/image.jpg");
+}
+
+
+function draw() {
+  var stepX = width  / (mouseX / 3 + 1);
+  var stepY = height / (mouseY / 3 + 1);
+  for (var gridY = 0; gridY < height; gridY += stepY){
+    for (var gridX = 0; gridX < width; gridX += stepX){
+      image(img, gridX, gridY, stepX, stepY);
+    }
+  }
+}
+
+function keyReleased(){
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+}
