59964a7cbc8d63141f43d9d6b4a17e9d5ea68c01
Niels NTG <NTG999@gmail.com>
2016-02-06 20:41:16 +0100

    Added P_4_1_2_01

---
+++ 01_P/P_4_1_2_01/sketch.js
@@ -0,0 +1,54 @@
+// P_4_1_2_01.pde
+//
+// Generative Gestaltung, ISBN: 978-3-87439-759-9
+// First Edition, Hermann Schmidt, Mainz, 2009
+// Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+// Copyright 2009 Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * image feedback process.
+ *
+ * KEYS
+ * del, backspace      : clear screen
+ * s                   : save png
+ */
+'use strict';
+
+var img;
+
+function setup() {
+  createCanvas(1024,780);
+  background(255);
+  img = loadImage('data/pic.png');
+  image(img,0,100);
+}
+
+function draw() {
+  var x1 = floor(random(0,width));
+  var y1 = 0;
+  var x2 = round(x1 + random(-7,7));
+  var y2 = round(random(-10,10));
+
+  var w = floor(random(35,50));
+  var h = height;
+  copy(img,x1,y1,w,h,x2,y2,w,h);
+}
+
+function keyReleased() {
+  if (code == 's' || code == 'S') saveCanvas(gd.timestamp(), 'png');
+  if (keyCode === DELETE || keyCode === BACKSPACE) {
+    background(255);
+    image(img,0,100);
+  }
+}
