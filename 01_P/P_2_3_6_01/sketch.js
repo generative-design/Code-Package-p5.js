7df748de4136b7c20f7c06011b74543a6744beec
Niels NTG <NTG999@gmail.com>
2016-02-18 08:26:49 +0100

    Added P_2_3_6_01

---
+++ 01_P/P_2_3_6_01/sketch.js
@@ -0,0 +1,156 @@
+// P_2_3_6_01.pde
+//
+// Generative Gestaltung, ISBN: 978-3-87439-759-9
+// First Edition, Hermann Schmidt, Mainz, 2009
+// Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+// Copyright 2009 Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * draw tool. draws a specific module according to
+ * its east, south, west and north neighbours.
+ *
+ * MOUSE
+ * drag left           : draw new module
+ * drag right          : delete a module
+ *
+ * KEYS
+ * del, backspace      : clear screen
+ * g                   : toggle show grid
+ * d                   : toggle show module values
+ * s                   : save png
+ */
+'use strict';
+
+var font;
+var modules = [];
+
+var tileSize = 30;
+var gridResolutionX;
+var gridResolutionY;
+var tiles = [];
+
+var doDrawGrid = true;
+var debugMode = false;
+
+function preload() {
+  for (var i = 0; i < 16; i++) {
+    modules[i] = loadImage("data/" + nf(i,2) + ".svg");
+  }
+}
+
+function setup() {
+  // use full screen size
+  createCanvas(windowWidth, windowHeight);
+
+  cursor(CROSS);
+  rectMode(CENTER);
+  imageMode(CENTER);
+  strokeWeight(0.15);
+  textSize(8);
+  textAlign(CENTER,CENTER);
+
+  gridResolutionX = round(width / tileSize) + 2;
+  gridResolutionY = round(height / tileSize) + 2;
+  initTiles();
+}
+
+function draw() {
+  background(255);
+
+  if (mouseIsPressed && mouseButton == LEFT) setTile();
+  if (mouseIsPressed && mouseButton == RIGHT) unSetTile();
+
+  if (doDrawGrid) drawGrid();
+  drawModules();
+}
+
+function initTiles() {
+  for (var gridX = 0; gridX < gridResolutionX; gridX++) {
+    tiles[gridX] = [];
+    for (var gridY = 0; gridY < gridResolutionY; gridY++) {
+      tiles[gridX][gridY] = 0;
+    }
+  }
+}
+
+function setTile() {
+  // convert mouse position to grid coordinates
+  var gridX = floor(mouseX / tileSize) + 1;
+  gridX = constrain(gridX, 1, gridResolutionX - 2);
+  var gridY = floor(mouseY / tileSize) + 1;
+  gridY = constrain(gridY, 1, gridResolutionY - 2);
+  tiles[gridX][gridY] = 1;
+}
+
+function unsetTile() {
+  var gridX = floor(mouseX / tileSize) + 1;
+  gridX = constrain(gridX, 1, gridResolutionX - 2);
+  var gridY = floor(mouseY / tileSize) + 1;
+  gridY = constrain(gridY, 1, gridResolutionY - 2);
+  tiles[gridX][gridY] = 0;
+}
+
+function drawGrid() {
+  for (var gridX = 0; gridX < gridResolutionX; gridX++) {
+    for (var gridY = 0; gridY < gridResolutionY; gridY++) {
+      var posX = tileSize * gridX - tileSize / 2;
+      var posY = tileSize * gridY - tileSize / 2;
+      fill(255);
+      if (debugMode) {
+        if (tiles[gridX][gridY] == 1) fill(220);
+      }
+      rect(posX, posY, tileSize, tileSize);
+    }
+  }
+}
+
+function drawModules() {
+  for (var gridX = 0; gridX < gridResolutionX - 1; gridX++) {
+    for (var gridY = 0; gridY < gridResolutionY - 1; gridY++) {
+      // use only active tiles
+      if (tiles[gridX][gridY] == 1) {
+        // check the four neightbours, each can be true or false
+        var NORTH = str(tiles[gridX][gridY - 1]);
+        var WEST = str(tiles[gridX - 1][gridY]);
+        var SOUTH = str(tiles[gridX][gridY + 1]);
+        var EAST = str(tiles[gridX + 1][gridY]);
+
+        // create binary result out of it
+        var binaryResult = NORTH + WEST + SOUTH + EAST;
+
+        // convert binary string to a decimal value from 0 - 15
+        var decimalResult = parseInt(binaryResult, 2);
+
+        var posX = tileSize * gridX - tileSize / 2;
+        var posY = tileSize * gridY - tileSize / 2;
+
+        // decimalResult is also the index for the shape array
+        image(modules[decimalResult], posX, posY, tileSize, tileSize);
+
+        if (debugMode) {
+          fill(150);
+          text(decimalResult + "\n" + binaryResult, posX, posY);
+        }
+      }
+    }
+  }
+}
+
+function keyPressed(){
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+
+  if (keyCode === DELETE || keyCode === BACKSPACE) initTiles();
+  if (key == 'g' || key == 'G') doDrawGrid = !doDrawGrid;
+  if (key == 'd' || key == 'D') debugMode = !debugMode;
+}
