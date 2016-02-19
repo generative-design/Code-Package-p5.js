45e6f1e0c0040cf43cef4377ba2de60d8e0a2856
Niels NTG <NTG999@gmail.com>
2016-02-19 18:02:17 +0100

    Added P_3_1_3_01

---
+++ 01_P/P_3_1_3_01/sketch.js
@@ -0,0 +1,110 @@
+// P_3_1_3_01.pde
+//
+// Generative Gestaltung, ISBN: 978-3-87439-759-9
+// First Edition, Hermann Schmidt, Mainz, 2009
+// Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+// Copyright 2009 Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * analysing and sorting the letters of a text
+ * changing the letters alpha value in relation to frequency
+ *
+ * MOUSE
+ * position x          : interpolate between normal text and sorted position
+ *
+ * KEYS
+ * a                   : toggle alpha mode
+ * s                   : save png
+ */
+'use strict';
+
+// var font;
+var joinedText;
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜß,.;:!? ";
+var counters = [];
+
+var charSize;
+var charColor;
+var posX;
+var posY;
+
+var drawAlpha = true;
+
+
+function preload() {
+  joinedText = loadStrings("data/faust_kurz.txt");
+}
+
+function setup() {
+  createCanvas(620, windowHeight);
+
+  noStroke();
+  textFont("monospace", 18);
+  // textSize(18);
+
+  joinedText = joinedText.join(joinedText, " ");
+  for (var i = 0; i < alphabet.length; i++) {
+    counters[i] = 0;
+  }
+
+  countCharacters();
+}
+
+function draw() {
+  background(255);
+
+  posX = 20;
+  posY = 40;
+
+  // go through all characters in the text to draw them
+  for (var i = 0; i < joinedText.length; i++) {
+    // again, find the index of the current letter in the alphabet
+    var upperCaseChar = joinedText.charAt(i).toUpperCase();
+    var index = alphabet.indexOf(upperCaseChar);
+    if (index < 0) continue;
+
+    if (drawAlpha) {
+      fill(87, 35, 129, counters[index] * 3);
+    } else {
+      fill(87, 35, 129);
+    }
+
+    var sortY = index * 20 + 40;
+    var m = map(mouseX, 50, width - 50, 0, 1);
+    m = constrain(m, 0, 1);
+    var interY = lerp(posY, sortY, m);
+
+    text(joinedText.charAt(i), posX, interY);
+
+    posX += textWidth(joinedText.charAt(i));
+    if (posX >= width - 200 && upperCaseChar == " ") {
+      posY += 30;
+      posX = 20;
+    }
+  }
+}
+
+function countCharacters() {
+  for (var i = 0; i < joinedText.length; i++) {
+    // get one character from the text and turn it to uppercase
+    var index = alphabet.indexOf(joinedText.charAt(i).toUpperCase());
+    // increacre the respective counter
+    if (index >= 0) counters[index]++;
+  }
+}
+
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  if (key == 'a' || key == 'S') drawAlpha = !drawAlpha;
+}
