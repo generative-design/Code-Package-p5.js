bb020573812c55a95a9fc621760de5ad23ffebd2
Niels NTG <NTG999@gmail.com>
2016-08-11 17:40:18 +0200

    Added M_6_1_01

--- 02_M/M_6_1_01/sketch.js
+++ 02_M/M_6_1_01/sketch.js
@@ -24,7 +24,6 @@
  * r             : reset positions
  * s             : save png
  */
-
 'use strict';
 
 var sketch = function( p ) {
@@ -77,6 +76,78 @@ var sketch = function( p ) {
         p.height - 5
       ));
     }
+  }
+
+  function Node(x, y, minX, maxX, minY, maxY) {
+    this.x = x;
+    this.y = y;
+    this.minX = minX;
+    this.maxX = maxX;
+    this.minY = minY;
+    this.maxY = maxY;
+    this.radius = 200; // Radius of impact
+    this.ramp = 1; // Influences the shape of the function
+    this.strength = -1; // Strength: positive value attracts, negative value repels
+    this.damping = 0.5;
+    this.velocity = p.createVector();
+    this.pVelocity = p.createVector();
+    this.maxVelocity = 10;
+
+    Node.prototype.attractNodes = function(nodeArray) {
+      for (var i = 0; i < nodeArray.length; i++) {
+        var otherNode = nodeArray[i];
+        // Stop when empty
+        if (otherNode === undefined) break;
+        // Continue from the top when node is itself
+        if (otherNode === this) continue;
+
+        this.attractNode(otherNode);
+      }
+    }
+
+    Node.prototype.attractNode = function(otherNode) {
+      var thisNodeVector = p.createVector(this.x, this.y);
+      var otherNodeVector = p.createVector(otherNode.x, otherNode.y);
+      var d = thisNodeVector.dist(otherNodeVector);
+
+      if (d > 0 && d < this.radius) {
+        var s = p.pow(d / this.radius, 1 / this.ramp);
+        var f = s * 9 * this.strength * (1 / (s + 1) + ((s - 3) / 4)) / d;
+        var df = thisNodeVector.sub(otherNodeVector);
+        df.mult(f);
+
+        otherNode.velocity.x += df.x;
+        otherNode.velocity.y += df.y;
+      }
+    }
+
+    Node.prototype.updateNode = function() {
+      this.velocity.limit(this.maxVelocity);
+
+      this.x += this.velocity.x;
+      this.y += this.velocity.y;
+
+      if (this.x < this.minX) {
+        this.x = this.minX - (this.x - this.minX);
+        this.velocity.x = -this.velocity.x;
+      }
+      if (this.x > this.maxX) {
+        this.x = this.maxX - (this.x - this.maxX);
+        this.velocity.x = -this.velocity.x;
+      }
+
+      if (this.y < this.minY) {
+        this.y = this.minY - (this.y - this.minY);
+        this.velocity.y = -this.velocity.y;
+      }
+      if (this.y > this.maxY) {
+        this.y = this.maxY - (this.y - this.maxY);
+        this.velocity.y = -this.velocity.y;
+      }
+
+      this.velocity.mult(1 - this.damping);
+    }
+
   }
 
 }
