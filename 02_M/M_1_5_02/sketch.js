39a7c1cc91bc070e8ba570655235e577795e02d8
Benedikt Groß <benedikt@looksgood.de>
2016-12-29 16:58:22 +0100

    introduced separate file for Agent class; small code style tweaks

--- 02_M/M_1_5_02/sketch.js
+++ 02_M/M_1_5_02/sketch.js
@@ -20,16 +20,15 @@
  * noise values (noise 2d) are used to animate a bunch of agents.
  *
  * KEYS
- * m                   : toogle menu open/close
  * 1-2                 : switch noise mode
  * space               : new noise seed
  * backspace           : clear screen
  * s                   : save png
  */
+
 'use strict';
 
 var sketch = function( p ) {
-
   var agents = [];
   var agentCount = 4000;
   var noiseScale = 300;
@@ -69,52 +68,10 @@ var sketch = function( p ) {
     if (p.key === '2') drawMode = 2;
     if (p.key === ' ') {
       var newNoiseSeed = p.floor(p.random(10000));
-      console.log('newNoiseSeed', newNoiseSeed);
       p.noiseSeed(newNoiseSeed);
     }
     if (p.keyCode === p.DELETE || p.keyCode === p.BACKSPACE) p.background(255);
   }
-
-  var Agent = function() {
-    this.vector = p.createVector(p.random(p.width), p.random(p.height));
-    this.vectorOld = this.vector.copy();
-    this.stepSize = p.random(1, 5);
-    this.isOutside = false;
-    this.angle;
-  }
-
-  Agent.prototype.update = function() {
-    this.vector.x += p.cos(this.angle) * this.stepSize;
-    this.vector.y += p.sin(this.angle) * this.stepSize;
-
-    this.isOutside = this.vector.x < 0 || this.vector.x > p.width || this.vector.y < 0 || this.vector.y > p.height;
-
-    if (this.isOutside) {
-      this.vector.set(p.random(p.width), p.random(p.height));
-      this.vectorOld = this.vector.copy();
-    }
-
-    p.strokeWeight(strokeWidth * this.stepSize);
-    p.line(this.vectorOld.x, this.vectorOld.y, this.vector.x, this.vector.y);
-
-    this.vectorOld = this.vector.copy();
-
-    this.isOutside = false;
-  }
-
-  Agent.prototype.update1 = function() {
-    this.angle = p.noise(this.vector.x / noiseScale, this.vector.y / noiseScale) * noiseStrength;
-
-    this.update();
-  }
-
-  Agent.prototype.update2 = function() {
-    this.angle = p.noise(this.vector.x / noiseScale, this.vector.y / noiseScale) * 24;
-    this.angle = (this.angle - p.floor(this.angle)) * noiseStrength;
-
-    this.update();
-  }
-
 };
 
 var myp5 = new p5(sketch);
