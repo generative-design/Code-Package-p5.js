f00f0f94b53a46c7dd5cc483ab00bad96021f0cf
Benedikt Groß <benedikt@looksgood.de>
2017-02-09 14:40:25 +0100

    fixed sketch and svg shapes

--- 01_P/P_2_1_1_04/sketch.js
+++ 01_P/P_2_1_1_04/sketch.js
@@ -1,5 +1,5 @@
 // P_2_1_1_04.js
-// 
+//
 // Generative Gestaltung, ISBN: 978-3-87439-759-9
 // First Edition, Hermann Schmidt, Mainz, 2009
 // Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
@@ -18,123 +18,102 @@
 
 /**
  * shapes in a grid, that are always facing the mouse
- * 	 
+ *
  * MOUSE
  * position x/y        : position to face
- * 
+ *
  * KEYS
  * 1-7                 : choose shapes
  * arrow up/down       : scale of shapes
  * arrow left/right    : additional rotation of shapes
- * c                   : toggle. color mode
  * d                   : toggle. size depending on distance
  * g                   : toggle. grid resolution
  * s                   : save png
- * p                   : save pdf
  */
- // shapes not overlapping
+
 'use strict';
+var currentShape;
+var shapes = [];
 
-	var savePDF = false;
+var tileCount = 10;
+var tileWidth;
+var tileHeight;
+var shapeSize = 50.0;
+var newShapeSize = shapeSize;
+var shapeAngle = 0.0;
+var maxDist;
 
-	var currentShape;
-
-	var tileCount = 10;
-	var tileWidth;
-	var tileHeight;
-	var shapeSize = 50.0;
-	var newShapeSize = shapeSize;
-	var shapeAngle = 0.0;
-	var maxDist;
-
-	var shapeColor = [0, 130, 164];
-	var fillMode = 0;
-	var sizeMode = 0;
+var sizeMode = 0;
 
 
-	function preload() {
-    	currentShape = loadImage("data/module_1.svg");
-  	}
+function preload() {
+  shapes.push(loadImage("data/module_1.svg"));
+  shapes.push(loadImage("data/module_2.svg"));
+  shapes.push(loadImage("data/module_3.svg"));
+  shapes.push(loadImage("data/module_4.svg"));
+  shapes.push(loadImage("data/module_5.svg"));
+  shapes.push(loadImage("data/module_6.svg"));
+  shapes.push(loadImage("data/module_7.svg"));
+  currentShape = shapes[0];
+}
 
-	function setup(){
-		createCanvas(600, 600);
-		smooth();
-		image(currentShape, 0, 0);
-		tileWidth = 600/tileCount;
-  		tileHeight = 600/tileCount;
-  		maxDist = Math.sqrt(Math.pow(600, 2) + Math.pow(600, 2));
-  		
-	};
+function setup(){
+  createCanvas(600, 600);
+  image(currentShape, 0, 0);
+  tileWidth = 600/tileCount;
+  tileHeight = 600/tileCount;
+  maxDist = Math.sqrt(Math.pow(600, 2) + Math.pow(600, 2));
+}
 
-	 function draw(){
-		if(savePDF){
-	  		beginRecord();
-	  	};
+function draw(){
+  background(255);
+  smooth();
 
-		background(255);
-		smooth();
+  for (var gridY = 0; gridY < tileCount; gridY++) {
+    for(var gridX = 0; gridX < tileCount; gridX++){
 
-		for (var gridY = 0; gridY < tileCount; gridY++) {
-			for(var gridX = 0; gridX < tileCount; gridX++){
+      var posX = tileWidth*gridX + tileWidth/2;
+      var posY = tileHeight*gridY + tileWidth/2;
 
-				var posX = tileWidth*gridX + tileWidth/2;
-				var posY = tileHeight*gridY + tileWidth/2;
+      // calculate angle between mouse position and actual position of the shape
+      var angle = Math.atan2(mouseY-posY, mouseX-posX) + (shapeAngle *(Math.PI/180));
 
-				// calculate angle between mouse position and actual position of the shape
-      			var angle = Math.atan2(mouseY-posY, mouseX-posX) + (shapeAngle *(Math.PI/180));
+      if (sizeMode == 0) newShapeSize = shapeSize;
+      if (sizeMode == 1) newShapeSize = shapeSize*1.5-map(dist(mouseX,mouseY,posX,posY),0,500,5,shapeSize);
+      if (sizeMode == 2) newShapeSize = map(dist(mouseX,mouseY,posX,posY),0,500,5,shapeSize);
 
-      			if(sizeMode == 0) newShapeSize = shapeSize;
-      			if(sizeMode == 1) newShapeSize = shapeSize*1.5-map(dist(mouseX,mouseY,posX,posY),0,500,5,shapeSize);
-      			if (sizeMode == 2) newShapeSize = map(dist(mouseX,mouseY,posX,posY),0,500,5,shapeSize);
-
-			    if (fillMode == 0) //currentShape.enableStyle();
-			    if (fillMode == 1) {
-			      	//currentShape.disableStyle();
-			        fill(shapeColor[0], shapeColor[1],shapeColor[2]);      
-			    }
-			    if (fillMode == 2) {
-			    	//currentShape.disableStyle();
-			        var a = map(dist(mouseX,mouseY,posX,posY), 0,maxDist, 255,0);
-			        fill(shapeColor[0], shapeColor[1],shapeColor[2]);       
-			    }
-			    if (fillMode == 3) {
-			        //currentShape.disableStyle();
-			        var a = map(dist(mouseX,mouseY,posX,posY), 0,maxDist, 0,255);
-			        fill(shapeColor[0], shapeColor[1],shapeColor[2]);      
-			    }
-
-			    push();
-			    translate(posX, posY);
-			    rotate(angle);
-			    //shapeMode(CENTER);
-
-			    noStroke();
-			    image(currentShape, 0,0, newShapeSize,newShapeSize);
-			    pop();
-			}
-		}
-
-		if (savePDF) {
-		    savePDF = false;
-		    endRecord();
-		}		
-	}
-
-	function keyReleased(){
-    if (key == 's' || key == 'S') p.saveCanvas(gd.timestamp(), 'png');
-    if (key == 'p' || key == 'P') savePDF = true;
-
-    if (key == '1') currentShape = loadImage("data/module_1.svg");
-    if (key == '2') currentShape = loadImage("data/module_2.svg");
-    if (key == '3') currentShape = loadImage("data/module_3.svg");
-    if (key == '4') currentShape = loadImage("data/module_4.svg");
-    if (key == '5') currentShape = loadImage("data/module_5.svg");
-    if (key == '6') currentShape = loadImage("data/module_6.svg");
-    if (key == '7') currentShape = loadImage("data/module_7.svg");
-
-    if (keyCode == UP_ARROW) shapeSize = shapeSize + 5;
-  	if (keyCode == DOWN_ARROW) shapeSize = Math.max(shapeSize-5, 5);
-  	if (keyCode == LEFT_ARROW) shapeAngle = shapeAngle + 5;
-  	if (keyCode == RIGHT_ARROW) shapeAngle = shapeAngle - 5;
-  	
+      push();
+      translate(posX, posY);
+      rotate(angle);
+      noStroke();
+      image(currentShape, 0,0, newShapeSize,newShapeSize);
+      pop();
+    }
   }
+}
+
+function keyReleased(){
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  if (key == 'd' || key == 'D') sizeMode = (sizeMode+1) % 3;
+  if (key == 'g' || key == 'G') {
+    tileCount = tileCount+5;
+    if (tileCount > 20) {
+      tileCount = 10;
+    }
+    tileWidth = width/tileCount;
+    tileHeight = height/tileCount;
+  }
+
+  if (key == '1') currentShape = shapes[0];
+  if (key == '2') currentShape = shapes[1];
+  if (key == '3') currentShape = shapes[2];
+  if (key == '4') currentShape = shapes[3];
+  if (key == '5') currentShape = shapes[4];
+  if (key == '6') currentShape = shapes[5];
+  if (key == '7') currentShape = shapes[6];
+
+  if (keyCode == UP_ARROW) shapeSize = shapeSize + 5;
+  if (keyCode == DOWN_ARROW) shapeSize = Math.max(shapeSize-5, 5);
+  if (keyCode == LEFT_ARROW) shapeAngle = shapeAngle + 5;
+  if (keyCode == RIGHT_ARROW) shapeAngle = shapeAngle - 5;
+}
