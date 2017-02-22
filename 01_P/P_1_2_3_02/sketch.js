0011fabb2e618d217cf40d14c7f80fd716b0ebd2
Benedikt Groß <benedikt@looksgood.de>
2017-02-22 14:00:11 +0100

    small code style tweaks

--- 01_P/P_1_2_3_02/sketch.js
+++ 01_P/P_1_2_3_02/sketch.js
@@ -26,20 +26,26 @@
  * s                   : save png
  * c                   : save color palette
  */
-'use strict';
+"use strict";
 
 var colorCount = 20;
 var hueValues = [];
 var saturationValues = [];
 var brightnessValues = [];
+var actRandomSeed = 0;
+
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(800,800);
   colorMode(HSB,360,100,100,100);
   noStroke();
 }
 
 function draw() {
+  noLoop();
+  background("grey");
+  randomSeed(actRandomSeed);
+
   // ------ colors ------
   // create palette
   for (var i=0; i<colorCount; i++) {
@@ -59,27 +65,27 @@ function draw() {
   // count tiles
   var counter = 0;
   // row count and row height
-  var rowCount = int(random(5,40));
+  var rowCount = int(random(5,30));
   var rowHeight = height/rowCount;
 
-  for(var i=0; i<rowCount; i++) {
-    // seperate each line in parts
+  // seperate each line in parts
+  for(var i=rowCount; i>=0; i--) {
     // how many fragments
     var partCount = i+1;
     var parts = [];
 
     for(var ii=0; ii<partCount; ii++) {
       // sub fragments or not?
-      if (random(1.0) < 0.075) {
+      if (random(1) < 0.075) {
         // take care of big values
         var fragments = int(random(2,20));
         partCount = partCount + fragments;
         for(var iii=0; iii<fragments; iii++) {
-          parts = append(parts, random(2));
+          parts.push(random(2));
         }
       }
       else {
-        parts = append(parts, random(2,20));
+        parts.push(random(2,20));
       }
     }
 
@@ -90,21 +96,25 @@ function draw() {
     // draw rects
     var sumPartsNow = 0;
     for(var ii=0; ii<parts.length; ii++) {
-      // get component color values
-      var index = counter % colorCount;
-      fill(hueValues[index],saturationValues[index],brightnessValues[index]);
-
       sumPartsNow += parts[ii];
-      rect(map(sumPartsNow, 0,sumPartsTotal, 0,width),rowHeight*i,
-      map(parts[ii], 0,sumPartsTotal, 0,width)*-1,rowHeight);
+
+      var x = map(sumPartsNow, 0,sumPartsTotal, 0,width);
+      var y = rowHeight*i;
+      var w = map(parts[ii], 0,sumPartsTotal, 0,width)*-1;
+      var h = rowHeight;
+
+      var index = counter % colorCount;
+      var col = color(hueValues[index],saturationValues[index],brightnessValues[index]);
+      fill(col);
+      rect(x, y, w, h);
 
       counter++;
     }
   }
-  noLoop();
 }
 
 function mouseReleased() {
+  actRandomSeed = int(random(100000));
   loop();
 }
 
@@ -116,6 +126,6 @@ function keyPressed() {
     for (var i=0; i<hueValues.length; i++) {
       colors.push(color(hueValues[i],saturationValues[i],brightnessValues[i]));
     }
-    writeFile([gd.ase.encode( colors )], gd.timestamp(), 'ase');
+    writeFile([gd.ase.encode(colors)], gd.timestamp(), 'ase');
   }
 }
