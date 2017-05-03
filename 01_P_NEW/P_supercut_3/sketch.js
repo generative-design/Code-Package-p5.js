42411d189ef7276a051990a2ef6155e04a3c8f99
Niels NTG <NTG999@gmail.com>
2017-05-03 14:58:19 +0200

    P_supercut_3: Added limerick supercut generator

---
+++ 01_P_NEW/P_supercut_3/sketch.js
@@ -0,0 +1,145 @@
+/**
+ * Create montage of video with a search query.
+ *
+ */
+"use strict";
+
+var videoSrc = 'data/video.mp4';
+var video;
+var subtitleSrc = 'data/subs.vtt';
+var subtitles;
+
+var searchQuery = 'day mission';
+var lexicon = new RiLexicon();
+
+var searchResults = [];
+var currentResult;
+
+var fragmentTimer;
+
+var gui;
+
+function preload() {
+  video = createVideo(videoSrc);
+  loadStrings(subtitleSrc, parseSubtitles);
+}
+
+function parseSubtitles(lines) {
+  subtitles = [];
+  var timecodeRegEx = new RegExp(/((\d{2}:){2}\d{2}(,|\.)\d{3})\s-->\s((\d{2}:){2}\d{2}(,|\.)\d{3})/);
+  var subtitleObject;
+  var startTime;
+  var endTime;
+  var dialog;
+  lines.forEach(function(line, i) {
+    if (timecodeRegEx.test(line) || i === lines.length) {
+
+      if (dialog) {
+        subtitles.push(new SubTitleObject(startTime, endTime, dialog));
+      }
+
+      if (i < lines.length) {
+        startTime = line.replace(/\s.+$/, '');
+        endTime = line.replace(/^.+\s/, '');
+        dialog = '';
+      }
+
+    } else {
+      if (startTime && endTime) {
+        dialog += line + ' ';
+      }
+    }
+  });
+  print(subtitles);
+}
+
+function SubTitleObject(startTime, endTime, dialog) {
+  this.startTimeStamp = startTime;
+  this.endTimeStamp = endTime;
+  this.startTime = getTimeInSeconds(startTime);
+  this.endTime = getTimeInSeconds(endTime);
+  this.dialog = dialog.replace(/\s\d+\s$|<(?:.)*?>/g, '').trim();
+  this.duration = this.endTime - this.startTime;
+  this.dialogWords = RiTa.tokenize(RiTa.stripPunctuation(this.dialog.toLowerCase()));
+}
+
+function getTimeInSeconds(timeString) {
+	var hours = parseInt(timeString.replace(/:.+$/, ''));
+	var minutes = parseInt(timeString.replace(/^\d.+?:|:\d.+$/, ''));
+	var seconds = parseInt(timeString.replace(/^\d.+:|(\,|\.).+$/, ''));
+	var milSeconds = parseInt(timeString.replace(/^.+(\,|\.)/, ''));
+	return (hours * 60 * 60) + (minutes * 60) + seconds + (milSeconds / 1000);
+}
+
+function findSubtiles(searchPattern) {
+  // AABBA
+  searchPattern = searchPattern.split(/\s+/);
+  var results = [];
+  subtitles.forEach(function(subtitle) {
+    if (results.length < 5) {
+      var lastWord = subtitle.dialogWords[subtitle.dialogWords.length - 1];
+      if (results.length === 0 || results.length === 1 || results.length === 4) {
+        if (lexicon.isRhyme(lastWord, searchPattern[0])) {
+          results.push(subtitle);
+        }
+      } else {
+        if (lexicon.isRhyme(lastWord, searchPattern[1])) {
+          results.push(subtitle);
+        }
+      }
+    }
+  });
+  return results;
+}
+
+function generateMontage() {
+  clearTimeout(fragmentTimer);
+  video.stop();
+
+  searchResults = findSubtiles(searchQuery);
+  print(
+    'Found ' + searchResults.length + ' results for search query ' + searchQuery,
+    searchResults
+  );
+
+  if (searchResults.length) {
+    queryResultMontage(searchResults, 0);
+  }
+}
+
+function queryResultMontage(searchResults, i) {
+  currentResult = searchResults[i];
+  var duration = currentResult.duration;
+  video.play();
+  video.time(currentResult.startTime);
+  print(currentResult.startTimeStamp, currentResult.dialog);
+  fragmentTimer = setTimeout(function() {
+    video.pause();
+    if (i < searchResults.length - 1) {
+      queryResultMontage(searchResults, i + 1);
+    } else {
+      clearTimeout(fragmentTimer);
+    }
+  }, duration * 1000);
+}
+
+function setSearchQuery(newSearchQuery) {
+  searchQuery = newSearchQuery;
+}
+
+function togglePlayback() {
+  clearTimeout(fragmentTimer);
+  if (video.elt.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function setup() {
+  noCanvas();
+
+  createGUI();
+
+  generateMontage(searchQuery);
+}
