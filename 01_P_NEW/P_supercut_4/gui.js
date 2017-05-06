addb2f41190b943f4838d69e54454c684af37ced
Niels NTG <NTG999@gmail.com>
2017-05-06 14:24:04 +0200

    P_supercut_4: Added 'every time searchQuery it goes faster' sketch

---
+++ 01_P_NEW/P_supercut_4/gui.js
@@ -0,0 +1,47 @@
+function createGUI() {
+  gui = QuickSettings.create(10, 10, 'P_supercut_4');
+  gui.addFileChooser('video', 'video file', 'video/*', selectVideoFile);
+  gui.addFileChooser('subtitles', 'subtitle file', undefined, selectSubtitleFile);
+  gui.addText('searchQuery', searchQuery, setSearchQuery);
+  gui.addButton('generateMontage', generateMontage);
+  gui.addProgressBar('searchResults', searchResults.length, 0, 'numbers');
+  gui.addProgressBar('video time', video.duration(), video.time(), 'numbers');
+  gui.addHTML('time', '00:00:00');
+  gui.addHTML('speed', '1');
+  gui.addHTML('dialog', '');
+  gui.addButton('togglePlayback', togglePlayback);
+}
+
+function updateGUI() {
+  gui.setProgressMax('searchResults', searchResults.length);
+  gui.setValue('searchResults', searchResults.indexOf(currentResult) + 1);
+  gui.setProgressMax('video time', round(video.duration()));
+  gui.setValue('video time', round(video.time()));
+  gui.setValue('speed', '<code>' + video.speed() + '</code>');
+  subtitles.forEach(function(subtitle) {
+    if (video.time() > subtitle.startTime && video.time() < subtitle.endTime) {
+      gui.setValue('time', '<code>' + subtitle.startTimeStamp + '</code>');
+      gui.setValue('dialog', '<code>' + subtitle.dialog + '</code>');
+    }
+  });
+}
+
+function setSearchQuery(newSearchQuery) {
+  searchQuery = newSearchQuery;
+}
+
+function selectVideoFile(file) {
+  videoSrc = URL.createObjectURL(file);
+  reset();
+}
+
+function selectSubtitleFile(file) {
+  subtitleSrc = URL.createObjectURL(file);
+  reset();
+}
+
+function reset() {
+  gui.destroy();
+  remove();
+  new p5;
+}
---
+++ 01_P_NEW/P_supercut_4/sketch.js
@@ -0,0 +1,131 @@
+/**
+ * Create montage of video with a search query.
+ */
+"use strict";
+
+var videoSrc = 'data/video.mp4';
+var video;
+var subtitleSrc = 'data/subs.vtt';
+var subtitles;
+
+var searchQuery = '\\b(comet)\\b';
+
+var searchResults = [];
+var currentResult;
+
+var fragmentTimer;
+
+var gui;
+
+function preload() {
+  video = createVideo(videoSrc);
+  loadStrings(subtitleSrc, parseSubtitles);
+}
+
+function parseSubtitles(lines) {
+  subtitles = [];
+  var timecodeRegEx = new RegExp(/((\d{2}:){2}\d{2}(,|\.)\d{3})\s-->\s((\d{2}:){2}\d{2}(,|\.)\d{3})/);
+  var subtitleObject;
+  var startTime;
+  var endTime;
+  var dialog;
+  lines.forEach(function(line, i) {
+    if (timecodeRegEx.test(line) || i === lines.length) {
+
+      if (dialog) {
+        subtitles.push(new SubTitleObject(startTime, endTime, dialog));
+      }
+
+      if (i < lines.length) {
+        startTime = line.replace(/\s.+$/, '');
+        endTime = line.replace(/^.+\s/, '');
+        dialog = '';
+      }
+
+    } else {
+      if (startTime && endTime) {
+        dialog += line + ' ';
+      }
+    }
+  });
+  print(subtitles);
+}
+
+function SubTitleObject(startTime, endTime, dialog) {
+  this.startTimeStamp = startTime;
+  this.endTimeStamp = endTime;
+  this.startTime = getTimeInSeconds(startTime);
+  this.endTime = getTimeInSeconds(endTime);
+  this.dialog = dialog.replace(/\s\d+\s$|<(?:.)*?>/g, '').trim();
+  this.duration = this.endTime - this.startTime;
+}
+
+function getTimeInSeconds(timeString) {
+	var hours = parseInt(timeString.replace(/:.+$/, ''));
+	var minutes = parseInt(timeString.replace(/^\d.+?:|:\d.+$/, ''));
+	var seconds = parseInt(timeString.replace(/^\d.+:|(\,|\.).+$/, ''));
+	var milSeconds = parseInt(timeString.replace(/^.+(\,|\.)/, ''));
+	return (hours * 60 * 60) + (minutes * 60) + seconds + (milSeconds / 1000);
+}
+
+function findSubtiles(searchPattern) {
+  searchPattern = new RegExp(searchPattern, 'i');
+  var results = [];
+  subtitles.forEach(function(subtitle) {
+    if (searchPattern.test(subtitle.dialog)) {
+      results.push(subtitle);
+    };
+  });
+  return results;
+}
+
+function generateMontage() {
+  clearTimeout(fragmentTimer);
+  video.stop();
+
+  searchResults = findSubtiles(searchQuery);
+  print(
+    'Found ' + searchResults.length + ' results for search query ' + searchQuery,
+    searchResults
+  );
+
+  if (searchResults.length) {
+    video.play();
+    video.speed(1);
+    queryResultMontage(searchResults, 0);
+  }
+}
+
+function queryResultMontage(searchResults, i) {
+  video.elt.ontimeupdate = function() {
+    updateGUI();
+    if (i < searchResults.length - 1) {
+      if (video.time() > searchResults[i].startTime) {
+        currentResult = searchResults[i];
+        video.speed(video.speed() / QUARTER_PI);
+        print(video.speed(), currentResult.startTimeStamp, currentResult.dialog);
+        i++;
+      }
+    } else {
+      video.elt.ontimeupdate = null;
+      return false;
+    };
+  }
+}
+
+function togglePlayback() {
+  clearTimeout(fragmentTimer);
+  if (video.elt.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function setup() {
+  noCanvas();
+
+  createGUI();
+
+  generateMontage(searchQuery);
+}
