2ffc530b50a0d784ac4fe89fedaf87a3b8065d2e
Niels NTG <NTG999@gmail.com>
2017-05-09 21:41:05 +0200

    P_moire_1: shift grid based on horizontal mouse position

--- 01_P_NEW/P_moire_1/sketch.js
+++ 01_P_NEW/P_moire_1/sketch.js
@@ -1,5 +1,5 @@
 /**
- * P_moire-trangles_1
+ * P_moire_1
  *
  * MOUSE
  *
@@ -9,7 +9,6 @@
  * c                   : save color palette
  */
 "use strict";
-var overlayspeed = 0;
 
 function setup() {
   createCanvas(600, 600);
@@ -19,8 +18,8 @@ function draw() {
   background(255);
 
   push();
-  translate(width*0.37, height/2);
-  for (var i = 0; i  < 360; i+= 12) {
+  translate(width * 0.37, height / 2);
+  for (var i = 0; i < 360; i += 12) {
     var x = cos(radians(i)) * 200;
     var y = sin(radians(i)) * 200;
     push();
@@ -30,42 +29,32 @@ function draw() {
   }
   pop();
 
-  overlayspeed += 0.25;
   push()
-  translate(overlayspeed, 0);
+  translate(mouseX, 0);
   overlay();
   pop();
-
-  if (overlayspeed=== width){
-    overlayspeed = 0;
-  }
-
 }
-
 
 function overlay() {
   strokeWeight(4);
 
-  for(var i = 0; i < width; i+= 5) {
+  for (var i = 0; i < width; i += 5) {
     line(i, 0, i, height);
   }
-
 }
-
 
 function triangleLines() {
   stroke(0);
   strokeWeight(2);
 
   var theight = 150;
-  for(var i = 0; i < theight; i+=5){
-    if(i < theight/2){
-      line(i, 0, i, i*-1 );
-    } else{
-      line(i, 0, i, (i*-1 + theight)*-1 )
+  for (var i = 0; i < theight; i += 5) {
+    if (i < theight / 2) {
+      line(i, 0, i, -i);
+    } else {
+      line(i, 0, i, -(-i + theight))
     }
   }
-
 }
 
 function keyPressed() {
