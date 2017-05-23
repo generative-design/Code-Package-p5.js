f54cfe403ea6488f6e02636e929dcd57295d8339
Benedikt Groß <benedikt@looksgood.de>
2017-05-23 17:15:49 +0200

    replaced 0 padding code with nf(), nice code style

--- 01_P/P_4_2_2_02/sketch.js
+++ 01_P/P_4_2_2_02/sketch.js
@@ -1,5 +1,5 @@
 // P_4_2_2_02.pde
-// 
+//
 // Generative Gestaltung, ISBN: 978-3-87439-759-9
 // First Edition, Hermann Schmidt, Mainz, 2009
 // Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
@@ -18,12 +18,12 @@
 
 /**
  * timelapse camera. after each intervalTime a picture is saved to the sketch folder
- * 
+ *
  */
 
 'use strict';
 
-var myCam; 
+var cam;
 
 // intervalTime in sec. here 5 min
 var intervalTime = 5*60;
@@ -32,33 +32,28 @@ var secondsSinceStart = 0;
 var startTime = gd.timestamp();
 var counter = 0;
 var doSave = true;
-
 var streamReady = false;
 
-function setup() { 
-  createCanvas(640, 480); 
-  myCam = createCapture(VIDEO, function(){streamReady = true});
-  myCam.hide();
-  
+function setup() {
+  createCanvas(640, 480);
+  cam = createCapture(VIDEO, function(){ streamReady = true });
+  cam.hide();
   noStroke();
-} 
+}
 
-function draw() { 
-  if(streamReady) { 
-    image(myCam, 0, 0, width, width*myCam.height/myCam.width); 
+function draw() {
+  if(streamReady) {
+    image(cam, 0, 0, width, width*cam.height/cam.width);
 
-    secondsSinceStart = millis() / 1000; 
+    secondsSinceStart = millis() / 1000;
     var interval = int(secondsSinceStart % intervalTime);
 
     if (interval == 0 && doSave == true) {
-      var string = "" + counter;
-      var pad = "00000";
-      var ans = pad.substring(0, pad.length - string.length) + string;
-      var saveFileName = str(startTime + "-" + ans);
+      var saveFileName = startTime + "-" + nf(counter, 5, 0);
       saveCanvas(saveFileName, 'png');
       doSave = false;
-      counter++; 
-    } 
+      counter++;
+    }
     else if (interval != 0) {
       doSave = true;
     }
@@ -66,5 +61,5 @@ function draw() {
     // visualize the time to the next shot
     fill(random(0,255),random(0,255),random(0,255));
     rect(map(interval, 0, intervalTime, 0, width), 0, 5, 5);
-  } 
+  }
 }
