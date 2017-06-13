cfd09e260cb19c63127a59b48767c6125f68cae7
Niels NTG <NTG999@gmail.com>
2017-06-13 14:58:18 +0200

    P_pendulum_2: Added pendulum sketch with realistic physics

---
+++ 01_P_NEW/P_pendulum_2/sketch.js
@@ -0,0 +1,206 @@
+// P_pendulum_2
+/**
+ * Drawing tool that moves a pendulum-esq contraption along paths drawn by the mouse.
+ * Each joint of the pendulum leaves behind its own trail.
+ *
+ * MOUSE
+ * mouse               : click and drag to create a path to draw a pendulum along with
+ *
+ * KEYS
+ * 1                   : toggle path line
+ * 2                   : toggle pendulum
+ * 3                   : toggle pendulum path
+ * arrow up            : increase amplitude
+ * arrow down          : decrease amplitude
+ * arrow left          : decrease speed
+ * arrow right         : increase speed
+ * s                   : save png
+ *
+ * CONTRIBUTED BY
+ * [Niels Poldervaart](http://NielsPoldervaart.nl)
+ */
+"use strict";
+
+var shapes = [];
+
+var newShape;
+
+var joints = 16;
+var amplitude = 64;
+var speed = 16;
+var resolution = 0.1;
+
+var showPath = true;
+var showPendulum = true;
+var showPendulumPath = true;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 360, 100, 100, 100);
+  noFill();
+  strokeWeight(1);
+  background(220);
+}
+
+function draw() {
+  background(0, 0, 100);
+
+  shapes.forEach(function(shape) {
+    shape.draw();
+    shape.update();
+  });
+
+  if (newShape) {
+    newShape.addPos(mouseX, mouseY);
+    newShape.draw();
+    newShape.update();
+  }
+}
+
+function Shape(amplitude, speed, resolution, joints) {
+  this.shapePath = [];
+  this.iterator = 0;
+  this.resolution = resolution;
+  this.pendulum = new Pendulum(amplitude, joints);
+  this.pendulumPath = [];
+
+  Shape.prototype.addPos = function(x, y) {
+    var newPos = createVector(x, y);
+    this.shapePath.push(newPos);
+  };
+
+  Shape.prototype.draw = function() {
+    strokeWeight(0.8);
+    stroke(0, 10);
+
+    if (showPath) {
+      beginShape();
+      this.shapePath.forEach(function(pos) {
+        vertex(pos.x, pos.y);
+      });
+      endShape();
+    }
+
+    var currentIndex = floor(this.iterator);
+
+    var currentPos = this.shapePath[currentIndex];
+    var previousPos = this.shapePath[currentIndex - 1];
+    if (previousPos) {
+
+      var offsetPos = p5.Vector.lerp(previousPos, currentPos, this.iterator - currentIndex);
+
+      this.pendulumPath.push(this.pendulum.getTrail(offsetPos));
+
+      push();
+
+      translate(offsetPos.x, offsetPos.y);
+      this.pendulum.update();
+      if (showPendulum) {
+        this.pendulum.draw();
+      }
+      pop();
+    }
+
+
+    if (showPendulumPath) {
+      beginShape();
+      this.pendulumPath.forEach(function(pos) {
+        vertex(pos.x, pos.y);
+      });
+      endShape();
+    }
+  };
+
+  Shape.prototype.update = function() {
+    this.iterator += this.resolution;
+    this.iterator = constrain(this.iterator, 0, this.shapePath.length);
+  };
+}
+
+function Pendulum(size, hierarchy) {
+  this.hierarchy = hierarchy - 1;
+  this.pendulumArm;
+  this.size = size;
+  this.angle = HALF_PI;
+  this.origin = createVector(0, 0);
+  this.end = this.origin.copy().setMag(this.size);
+  this.gravity = 0.04;
+  this.damping = 0.998;
+  this.angularAcceleration = 0;
+  this.angularVelocity = 0;
+
+  if (this.hierarchy > 0) {
+    this.pendulumArm = new Pendulum(this.size / 1.5, this.hierarchy);
+  }
+
+  Pendulum.prototype.update = function() {
+    this.end.set(this.origin.x + this.size * sin(this.angle), this.origin.y + this.size * cos(this.angle));
+
+    this.angularAcceleration = (-this.gravity / this.size) * sin(this.angle);
+    this.angle += this.angularVelocity;
+    this.angularVelocity += this.angularAcceleration;
+    this.angularVelocity *= this.damping;
+
+    if (this.pendulumArm) {
+      this.pendulumArm.update();
+    }
+  };
+
+  Pendulum.prototype.getTrail = function(offset, end) {
+    if (this.pendulumArm) {
+      if (end) {
+        end.add(this.end)
+      } else {
+        end = this.end.copy();
+      }
+      return this.pendulumArm.getTrail(offset, end);
+    } else {
+      return this.end.copy().add(end).add(offset);
+    }
+  };
+
+  Pendulum.prototype.draw = function() {
+    stroke(0, 40);
+    beginShape();
+    vertex(this.origin.x, this.origin.y);
+    vertex(this.end.x, this.end.y);
+    endShape();
+
+    if (this.pendulumArm) {
+      push();
+      translate(this.end.x, this.end.y);
+      this.pendulumArm.draw();
+      pop();
+    }
+  };
+
+}
+
+function mousePressed() {
+  newShape = new Shape(amplitude, speed, resolution, joints);
+  newShape.addPos(mouseX, mouseY);
+}
+
+function mouseReleased() {
+  shapes.push(newShape);
+  newShape = undefined;
+}
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+
+  if (key == ' ') {
+    shapes = [];
+    background(255);
+    loop();
+  }
+
+  if (key == '1') showPath = !showPath;
+  if (key == '2') showPendulum = !showPendulum;
+  if (key == '3') showPendulumPath = !showPendulumPath;
+
+  if (keyCode == UP_ARROW) amplitude += 2;
+  if (keyCode == DOWN_ARROW) amplitude -= 2;
+  if (keyCode == LEFT_ARROW) speed--;
+  if (keyCode == RIGHT_ARROW) speed++;
+}
