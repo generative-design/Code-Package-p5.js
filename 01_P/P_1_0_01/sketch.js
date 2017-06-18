3b1264d94046814bc91d2380fb29e7aa365c0edb
Niels NTG <NTG999@gmail.com>
2017-06-18 17:54:19 +0200

    Improved code style

--- 01_P/P_1_0_01/sketch.js
+++ 01_P/P_1_0_01/sketch.js
@@ -37,10 +37,10 @@ function setup() {
 }
 
 function draw() {
-  background(mouseY/2, 100, 100);
+  background(mouseY / 2, 100, 100);
 
-  fill(360-mouseY/2, 100, 100);
-  rect(360, 360, mouseX+1, mouseX+1);
+  fill(360 - mouseY / 2, 100, 100);
+  rect(360, 360, mouseX + 1, mouseX + 1);
 }
 
 function keyPressed() {
