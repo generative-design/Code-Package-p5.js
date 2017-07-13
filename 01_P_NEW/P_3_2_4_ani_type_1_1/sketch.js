55f0652cc328a16c73511dd298388b3bc5910feb
Joey Lee <josephkanglee@gmail.com>
2017-07-13 23:50:33 +0200

    prelim steps to having line breaks

--- 01_P_NEW/P_3_2_4_ani_type_1_1/sketch.js
+++ 01_P_NEW/P_3_2_4_ani_type_1_1/sketch.js
@@ -15,6 +15,7 @@
 "use strict";
 
 var textTyped;
+var textTypedCounter;
 var font;
 var fontSize;
 var style;
@@ -28,7 +29,9 @@ function setup() {
   noLoop();
 
   // assign globals
-  textTyped = "Type ...!"
+  textTyped = [];
+  textTyped.push("Type...!");
+  textTypedCounter = 0;
   fontSize = 120;
   style = 1;
   ranges = [];
@@ -36,7 +39,7 @@ function setup() {
 
   rectMode(CENTER);
 
-  opentype.load('data/FreeSans.otf', function(err, f) {
+  opentype.load('../../data/FreeSans.otf', function(err, f) {
     if (err) {
       print('Font could not be loaded: ' + err);
     } else {
@@ -47,7 +50,13 @@ function setup() {
 }
 
 
+// create an array of strings
+// each string is the line
+// if enter is pressed, create a new string,
+// and move to a new line by editing the svg "m" values
+
 function draw() {
+  // noLoop();
   if (!font) return;
   background(255, 255, 255, 20);
   // margin border
@@ -55,43 +64,47 @@ function draw() {
 
   fill(0);
 
-  if (textTyped.length > 0) {
-    // get a path from OpenType.js
-    var fontPath = font.getPath(textTyped, 0, 0, fontSize);
-    // convert it to a g.Path object
-    path = new g.Path(fontPath.commands);
-    // resample it with equidistant points
-    path = g.resampleByLength(path, 1);
+  textTyped.forEach(function(txt, lineNum){
+    if (txt.length > 0) {
+      // get a path from OpenType.js
+      var fontPath = font.getPath(txt, 0, 0, fontSize);
+      // convert it to a g.Path object
+      path = new g.Path(fontPath.commands);
+      // resample it with equidistant points
+      path = g.resampleByLength(path, 1);
 
-    var pathsCount = path.commands.length;
-    breaks = pathsCount / textTyped.length;
 
-    ranges = [];
-    for(var i = 0; i < pathsCount-1; i+=breaks){
-      ranges.push({start:floor(i)});
-    }
+      var pathsCount = path.commands.length;
+      breaks = pathsCount / txt.length;
 
-    ranges.forEach(function(d){
-      if(counter < breaks){
-        var cmd = path.commands[counter+d.start];
-        var ocmd = path.commands[ceil(breaks) - counter + d.start];
-        if(style == 1){
-          ellipse(cmd.x, cmd.y, fontSize*0.10, fontSize*0.10);
-        }
-        if(style == 2){
-         rect(cmd.x, cmd.y, fontSize*0.10, fontSize*0.10);
-        }
-        if(style == 3){
-          line(cmd.x, cmd.y, ocmd.x , ocmd.y);
-        }
-
-        counter++;
-      } else{
-        counter=0;
+      ranges = [];
+      for(var i = 0; i < pathsCount-1; i+=breaks){
+        ranges.push({start:floor(i)});
       }
 
-    })
-  }
+      ranges.forEach(function(d){
+        if(counter < breaks){
+          var cmd = path.commands[counter+d.start];
+          var ocmd = path.commands[ceil(breaks) - counter + d.start];
+          if(style == 1){
+            ellipse(cmd.x, cmd.y + (lineNum*fontSize), fontSize*0.10, fontSize*0.10);
+          }
+          if(style == 2){
+           rect(cmd.x, cmd.y + (lineNum*fontSize), fontSize*0.10, fontSize*0.10);
+          }
+          if(style == 3){
+            line(cmd.x, cmd.y+ (lineNum*fontSize), ocmd.x , ocmd.y+ (lineNum*fontSize));
+          }
+
+          counter++;
+        } else{
+          counter=0;
+        }
+
+      })
+    }
+  })
+
 }
 
 function keyPressed() {
@@ -99,8 +112,14 @@ function keyPressed() {
 
   if (keyCode === DELETE || keyCode === BACKSPACE || keyCode === LEFT_ARROW) {
     textTyped = textTyped.substring(0,max(0,textTyped.length-1));
+
+    // remove the last letter and destroy each string in the array
+    // until you kill all the strings
   } else if (keyCode === TAB || keyCode === ENTER || keyCode === RETURN || keyCode === ESCAPE) {
     // do nothing
+    console.log("enter!")
+    textTyped.push("");
+    textTypedCounter++;
   } else {
     if(key == "1"){
       style = 1;
@@ -112,7 +131,7 @@ function keyPressed() {
       style = 3;
     }
 
-    textTyped += key;
+    textTyped[textTypedCounter] += key;
   }
   loop();
 }
