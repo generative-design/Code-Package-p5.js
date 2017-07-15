761081ea53cd9669cd2a793da7bcd2f7b6af395d
Niels NTG <NTG999@gmail.com>
2017-07-15 17:11:35 +0200

    P_moire_8: Finished moire typography sketch

--- 01_P_NEW/P_3_2_5_type_moire_8/sketch.js
+++ 01_P_NEW/P_3_2_5_type_moire_8/sketch.js
@@ -1,4 +1,4 @@
-// P_moire_8
+// P_3_2_5_type_moire_8
 /**
  * Drawing tool for creating moire effect compositions using
  * smooth path of any length, width, smoothness and colour.
@@ -7,15 +7,12 @@
  * mouse               : click and drag to draw moire pattern path
  *
  * KEYS
- * 1                   : set color to red
- * 2                   : set color to green
- * 3                   : set color to blue
- * 4                   : set color to black
+ * enter               : redraw
  * arrow right         : increase smoothness
  * arrow left          : decrease smoothness
- * arrow up            : increase rectangle width
- * arrow down          : decrease rectangle width
- * s                   : save png
+ * arrow up            : increase ribbon width
+ * arrow down          : decrease ribbon width
+ * control             : save png
  *
  * CONTRIBUTED BY
  * [Niels Poldervaart](http://NielsPoldervaart.nl)
@@ -24,55 +21,66 @@
 
 var shapes = [];
 var density = 2.5;
-var shapeHeight = 39.5;
+var ribbonWidth = 92;
 var shapeColor;
 var smoothness = 8;
+var fontSize = 800;
+var pathSimplification = 0.5;
+var randomPathSimplification = true;
 
-var newShape;
+var textTyped = 'moire';
 
 var font;
 
 function preload() {
-  font = loadFont('Oval Black.otf');
+  font = loadFont('NotoSans-Bold.ttf');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noFill();
   strokeWeight(1);
-  shapeColor = color(255);
-  textAlign(CENTER, CENTER);
-  textFont(font, 100);
+  shapeColor = color(0);
+  textFont(font, fontSize);
 
-  shapes.push(new Shape(shapeHeight, shapeColor, '4', 0, height / 2));
-  shapes.push(new Shape(shapeHeight, shapeColor, '8', 250, height / 2));
-  shapes.push(new Shape(shapeHeight, shapeColor, '9', 500, height / 2));
+  createShapes();
 }
 
 function draw() {
-  background(80);
+  background(255);
+
+  translate(ribbonWidth, (fontSize / 4) + (height / 2));
 
   shapes.forEach(function(shape) {
-    shape.h = shapeHeight;
     shape.draw();
   });
 }
 
-function Shape(h, c, t, x, y) {
+function createShapes() {
+  shapes = textTyped.split('').map(function(char, index) {
+    var x = index > 0 ? font.textBounds(textTyped.substring(0, index), 0, 0, fontSize).w : 0;
+    return new Shape(x, 0, ribbonWidth, shapeColor, char);
+  });
+}
+
+function Shape(x, y, h, color, char) {
   this.shapePath = [];
-  this.h = h;
-  this.c = c;
-  this.t = t;
+  this.x = x;
+  this.y = y;
+  this.color = color;
+  this.char = char;
 
   this.shapePath = font.textToPoints(
-    this.t, x, y, 600,
-    // {simplifyThreshold: 0.01}
+    this.char, this.x, this.y, fontSize,
+    {
+      simplifyThreshold: randomPathSimplification ? random(pathSimplification) : pathSimplification
+    }
   );
   this.shapePath.push(this.shapePath[2]);
 
   Shape.prototype.draw = function() {
-    stroke(this.c);
-    for (var i = 0; i < this.h; i += density) {
+    stroke(this.color);
+    for (var i = -ribbonWidth; i < 0; i += density) {
       beginShape();
       this.shapePath.forEach(function(pos, index, shapePath) {
         var previousPos = shapePath[index - 1];
@@ -90,22 +98,30 @@ function Shape(h, c, t, x, y) {
   };
 }
 
-function keyPressed() {
-  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-
-  if (key == '1') shapeColor = color(255, 0, 0);
-  if (key == '2') shapeColor = color(0, 255, 0);
-  if (key == '3') shapeColor = color(0, 0, 255);
-  if (key == '4') shapeColor = color(255);
-
-  if (key == ' ') {
-    shapes = [];
-    redraw();
-  }
+function keyReleased() {
+  if (keyCode === CONTROL) saveCanvas(gd.timestamp(), 'png');
 
   if (keyCode === RIGHT_ARROW) smoothness += density;
   if (keyCode === LEFT_ARROW) smoothness -= density;
 
-  if (keyCode === UP_ARROW) shapeHeight += density;
-  if (keyCode === DOWN_ARROW) shapeHeight -= density;
+  if (keyCode === UP_ARROW) ribbonWidth += density;
+  if (keyCode === DOWN_ARROW) ribbonWidth -= density;
+
+  if (keyCode === ENTER) createShapes();
+}
+
+function keyPressed() {
+  if (keyCode === DELETE || keyCode === BACKSPACE) {
+    if (textTyped.length > 0) {
+      textTyped = textTyped.substring(0, textTyped.length - 1);
+      createShapes();
+    }
+  }
+}
+
+function keyTyped() {
+  if (keyCode >= 32) {
+    textTyped += key;
+    createShapes();
+  }
 }
