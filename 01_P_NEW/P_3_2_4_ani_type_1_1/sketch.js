e2f4d980749d7db9f0bd666818c5126855963c7b
Joey Lee <josephkanglee@gmail.com>
2017-07-23 19:48:11 +0200

    fixed missing point issue
    each line requires its own counter to animate properly

--- 01_P_NEW/P_3_2_4_ani_type_1_1/sketch.js
+++ 01_P_NEW/P_3_2_4_ani_type_1_1/sketch.js
@@ -8,6 +8,7 @@
  * 2                         : animated squares
  * 3                         : animated lines
  * left arrow                : remove the letter
+ * enter                     : new line
  *
  * CONTRIBUTED BY
  * [Joey Lee](http://jk-lee.com)
@@ -20,9 +21,10 @@ var font;
 var fontSize;
 var style;
 var path;
+var paths;
 var ranges;
 var breaks;
-var counter;
+var counters;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -30,12 +32,15 @@ function setup() {
 
   // assign globals
   textTyped = [];
-  textTyped.push("Type...!");
+  counters = [];
+  textTyped.push("HELLO");
+  counters.push(0);
+  textTyped.push("WORLD");
+  counters.push(0);
   textTypedCounter = 0;
   fontSize = 120;
   style = 1;
-  ranges = [];
-  counter = 0;
+
 
   rectMode(CENTER);
 
@@ -51,59 +56,83 @@ function setup() {
 
 
 // create an array of strings
-// each string is the line
+// each string is a line
 // if enter is pressed, create a new string,
 // and move to a new line using on the cmd.x, cmd.y
 
 function draw() {
   // noLoop();
   if (!font) return;
-  background(255, 255, 255, 20);
+  background(255, 255, 255, 30);
   // margin border
   translate(20,220);
 
   fill(0);
 
+
+  // each line of text needs to be animating on its own
+  paths = [];
+
   textTyped.forEach(function(txt, lineNum){
-    if (txt.length > 0) {
-      // get a path from OpenType.js
+    if(txt.length > 0){
       var fontPath = font.getPath(txt, 0, 0, fontSize);
       // convert it to a g.Path object
       path = new g.Path(fontPath.commands);
       // resample it with equidistant points
       path = g.resampleByLength(path, 1);
 
+      var output = {
+        data: path,
+        lineNumber: lineNum,
+        len: path.commands.length,
+        breaks: floor(path.commands.length / txt.length)
+      };
+      paths.push(output);
 
-      var pathsCount = path.commands.length;
-      breaks = pathsCount / txt.length;
 
-      ranges = [];
-      for(var i = 0; i < pathsCount-1; i+=breaks){
-        ranges.push({start:floor(i)});
-      }
 
-      ranges.forEach(function(d){
-        if(counter < breaks){
-          var cmd = path.commands[counter+d.start];
-          var ocmd = path.commands[ceil(breaks) - counter + d.start];
-          if(style == 1){
-            ellipse(cmd.x, cmd.y + (lineNum*fontSize), fontSize*0.10, fontSize*0.10);
-          }
-          if(style == 2){
-           rect(cmd.x, cmd.y + (lineNum*fontSize), fontSize*0.10, fontSize*0.10);
-          }
-          if(style == 3){
-            line(cmd.x, cmd.y+ (lineNum*fontSize), ocmd.x , ocmd.y+ (lineNum*fontSize));
-          }
-
-          counter++;
-        } else{
-          counter=0;
-        }
-
-      })
     }
   })
+
+
+  ranges = [];
+  paths.forEach(function(path, index){
+    var output = {id: index, start:[]};
+    // console.log(path.len)
+    for(var i = 0; i < path.len-1; i+=path.breaks){
+      output.start.push(floor(i));
+    }
+    ranges.push(output);
+  })
+
+
+
+  ranges.forEach(function(range, i){
+    // console.log(range.start);
+    range.start.forEach(function(d){
+    if(counters[i] < paths[i].breaks){
+        var cmd = paths[i].data.commands[counters[i]+ d];
+        var ocmd = paths[i].data.commands[ceil(paths[i].breaks) - counters[i] + d];
+        if(cmd !=undefined && ocmd != undefined){
+
+          if(style == 1){
+            ellipse(cmd.x, cmd.y + (paths[i].lineNumber*fontSize), fontSize*0.10, fontSize*0.10);
+          }
+          if(style == 2){
+           rect(cmd.x, cmd.y + (paths[i].lineNumber*fontSize), fontSize*0.10, fontSize*0.10);
+          }
+          if(style == 3){
+            line(cmd.x, cmd.y+ (paths[i].lineNumber*fontSize), ocmd.x , ocmd.y+ (paths[i].lineNumber*fontSize));
+          }
+        }
+      counters[i]++;
+    }else{
+      counters[i] = 0;
+    }
+    })
+
+  })
+
 
 }
 
@@ -111,15 +140,8 @@ function keyPressed() {
   if (keyCode === CONTROL) saveCanvas(gd.timestamp(), 'png');
 
   if (keyCode === DELETE || keyCode === BACKSPACE || keyCode === LEFT_ARROW) {
-    // textTyped = textTyped.substring(0,max(0,textTyped.length-1));
-
     // remove the last letter and destroy each string in the array
     // until you kill all the strings
-    console.log("counter:", textTypedCounter)
-    console.log("textyped", textTyped)
-    console.log("textyped length", textTyped.length)
-    // if at index 0 and length == 0; don't do anything
-    // otherwise, keep removing items
 
     if (textTypedCounter >= 0 && textTyped[0].length > 0){
      textTyped[textTypedCounter] = textTyped[textTypedCounter].substring(0,max(0,textTyped[textTypedCounter].length-1));
@@ -129,19 +151,15 @@ function keyPressed() {
 
     if(textTyped[textTypedCounter].length == 0){
         textTypedCounter--;
-
         if(textTypedCounter < 0){
           console.log("nothing left")
           textTypedCounter = 0;
         }else{
           textTyped.pop();
         }
-
     }
 
-
   } else if (keyCode === TAB || keyCode === ENTER || keyCode === RETURN || keyCode === ESCAPE) {
-    // do nothing
     console.log("enter!")
     textTyped.push("");
     textTypedCounter++;
@@ -149,14 +167,15 @@ function keyPressed() {
     if(key == "1"){
       style = 1;
     }
-    if(key == "2"){
+    else if(key == "2"){
       style = 2;
     }
-    if(key == "3"){
+    else if(key == "3"){
       style = 3;
+    }else{
+      textTyped[textTypedCounter] += key;
     }
-
-    textTyped[textTypedCounter] += key;
+    loop()
   }
-  loop();
+
 }
