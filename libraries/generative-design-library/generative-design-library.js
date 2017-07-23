92c1364bf410ea857f26f9290adaec0efdb4b3f6
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2017-07-23 18:24:19 +0200

    Added new version of generative-design-library.js

--- libraries/generative-design-library/generative-design-library.js
+++ libraries/generative-design-library/generative-design-library.js
@@ -163,7 +163,7 @@ var packageInfo = require('./package.json');
 var color = require('./color');
 var WacomTablet = require('./WacomTablet.js');
 
-console.log("%c * "+ packageInfo.name +" "+ packageInfo.version + " * ", "background: #948816; color: #fff");
+console.log("%c * "+ packageInfo.name +" "+ packageInfo.version + " * ", "background: #3c3; color: black");
 
 /**
  * Returns neatly formated timestamp as string e.g. "151025_172441_790"
@@ -174,7 +174,7 @@ console.log("%c * "+ packageInfo.name +" "+ packageInfo.version + " * ", "backgr
 var timestamp = function(){
   var date = new Date();
   var year = date.getFullYear();
-  var month = date.getMonth()+1;
+  var month = date.getMonth() + 1;
   var day = date.getDate();
   var hour = date.getHours();
   var minute = date.getMinutes();
@@ -182,12 +182,12 @@ var timestamp = function(){
   var millis = date.getMilliseconds();
 
   year = year.toString().substr(2);
-  month = ("00" + month).substr(-2,2);
-  day = ("00" + day).substr(-2,2);
-  minute = ("00" + minute).substr(-2,2);
-  second = ("00" + second).substr(-2,2);
+  month = ("00" + month).substr(-2, 2);
+  day = ("00" + day).substr(-2, 2);
+  minute = ("00" + minute).substr(-2, 2);
+  second = ("00" + second).substr(-2, 2);
 
-  return [year,month,day,"_",hour,minute,second,"_",millis].join('');
+  return [year, month, day, "_", hour, minute, second, "_", millis].join('');
 };
 
 
