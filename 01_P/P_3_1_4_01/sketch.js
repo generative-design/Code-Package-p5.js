14e1fbb64940db014371d60eb753b3fd1f4cf007
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2017-07-27 19:12:21 +0200

    P_3_2_4_01: Added header and renamed files

--- 01_P/P_3_1_4_01/sketch.js
+++ 01_P/P_3_1_4_01/sketch.js
@@ -1,7 +1,31 @@
-// P_3_1_4_01
+// P_3_1_4_01.pde
+//
+// Generative Gestaltung, ISBN: 978-3-87439-759-9
+// First Edition, Hermann Schmidt, Mainz, 2009
+// Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+// Copyright 2009 Hartmut Bohnacker, Benedikt Gross, Julia Laub, Claudius Lazzeroni
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /**
- * treemap test
+ * counting the words of a text and display them in a treemap diagram.
  *
+ *
+ * KEYS
+ * 1                   : toggle sort mode
+ * 2                   : only horizontal rows
+ * 3                   : only vertical rows
+ * 4                   : both kind of rows rows
+ * s                   : save png
  */
 'use strict';
 
