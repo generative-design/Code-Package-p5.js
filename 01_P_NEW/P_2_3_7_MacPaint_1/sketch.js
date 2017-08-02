4c0a0ac802e8265b3135f590e16715cce20d3533
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2017-08-02 16:49:23 +0200

    P_2_3_7_MacPaint: Added some new functions

--- 01_P_NEW/P_2_3_7_MacPaint_1/sketch.js
+++ 01_P_NEW/P_2_3_7_MacPaint_1/sketch.js
@@ -1,15 +1,22 @@
 // P_MacPaint_1
 /**
- * Simple drawing tool where mouse input gets mirrored over multiple axis
+ * Simple drawing tool where mouse input gets mirrored over multiple axes
  *
  * MOUSE
  * left click          : draw line
- * scroll wheel        : change stroke weight
  *
  * KEYS
  * 1                   : toggle vertical mirror
  * 2                   : toggle horizontal mirror
- * 3                   : toggle diagonal mirror
+ * 3                   : toggle diagonal mirror 1
+ * 4                   : toggle diagonal mirror 2
+ * 5-9                 : change color
+ * 0                   : color white (eraser)
+ * arrow up            : increase line weight
+ * arrow down          : decrease line weight
+ * arrow right         : increase number of tiles
+ * arrow left          : decrease number of tiles
+ * d                   : show/hide mirror axes
  * del, backspace      : clear screen
  * s                   : save png
  *
@@ -22,49 +29,75 @@ var gif;
 var canvasElement;
 var recording = false;
 
-var lineWidth = 1;
+var lineWidth = 3;
+var lineColor;
 
-var verticalMirror = true;
-var horizontalMirror = true;
-var diagonalMirror = true;
+var mv = true;
+var mh = true;
+var md1 = true;
+var md2 = true;
+var penCount = 1;
+
+var showAxes = true;
+
+var img;
 
 function setup() {
+  // Please work with a square canvas
   canvasElement = createCanvas(800, 800);
-  cursor(CROSS);
+  noCursor();
   noFill();
-  strokeWeight(lineWidth);
+  lineColor = color(0);
+
+  // Create an offscreen graphics object to draw into
+  img = createGraphics(width, height);
+  img.pixelDensity(1);
 
   setupGIF();
 }
 
 function draw() {
-  strokeWeight(lineWidth);
+  background(255);
+  image(img, 0, 0);
+
+  img.strokeWeight(lineWidth);
+  img.stroke(lineColor);
 
   if (mouseIsPressed) {
+    for (var i = 0; i < penCount; i++) {
+      for (var j = 0; j < penCount; j++) {
+        var w = width;
+        var h = height;
+        var x = (mouseX + i * w / penCount);
+        var y = (mouseY + j * h / penCount);
+        var px = (pmouseX + i * w / penCount);
+        var py = (pmouseY + j * h / penCount);
 
-    // Mirror mouse input along vertical axis.
-    var mirrorX = verticalMirror ? width - mouseX : mouseX;
-    var pMirrorX = verticalMirror ? width - pmouseX : pmouseX;
+        if (x > w) {
+          x -= w;
+          px -= w;
+        }
 
-    // Mirror mouse input along horizontal axis.
-    var mirrorY = horizontalMirror ? height - mouseY : mouseY;
-    var pMirrorY = horizontalMirror ? height - pmouseY : pmouseY;
+        if (y > h) {
+          y -= h;
+          py -= h;
+        }
 
-    // Draw line at current mouse position.
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    // Draw line at mirrored position.
-    line(mirrorX, mirrorY, pMirrorX, pMirrorY);
-    // Draw line at vertically mirrored position.
-    line(mirrorX, mouseY, pMirrorX, pmouseY);
-    // Draw line at horizontally mirrored position.
-    line(mouseX, mirrorY, pmouseX, pMirrorY);
+        // Normal position
+        img.line(x, y, px, py); 
+        // Horizontal mirror or all three other mirrors
+        if (mh || md2 && md1 && mv) img.line(w - x, y, w - px, py);
+        // Vertical mirror
+        if (mv || md2 && md1 && mh) img.line(x, h - y, px, h - py);
+        // Horizontal and vertical mirror
+        if (mv && mh || md2 && md1) img.line(w - x, h - y, w - px, h - py);
 
-    // When mirroring diagonally, flip X and Y inputs.
-    if (diagonalMirror) {
-      line(mouseY, mouseX, pmouseY, pmouseX);
-      line(mirrorY, mirrorX, pMirrorY, pMirrorX);
-      line(mirrorY, mouseX, pMirrorY, pmouseX);
-      line(mouseY, mirrorX, pmouseY, pMirrorX);
+        // When mirroring diagonally, flip X and Y inputs.
+        if (md1 || md2 && mv && mh) img.line(y, x, py, px);
+        if (md1 && mh || md2 && mv) img.line(y, w - x, py, w - px);
+        if (md1 && mv || md2 && mh) img.line(h - y, x, h - py, px);
+        if (md1 && mv && mh || md2) img.line(h - y, w - x, h - py, w - px);
+      }
     }
 
     if (recording) {
@@ -73,19 +106,64 @@ function draw() {
 
   }
 
+  if (showAxes) {
+    var w = width / penCount;
+    var h = height / penCount;
+
+    // draw mirror axes
+    stroke(0, 50);
+    strokeWeight(1);
+    for (var i = 0; i < penCount; i++) {
+      for (var j = 0; j < penCount; j++) {
+        var x = i * w;
+        var y = j * h;
+
+        if (mh) line(x + w / 2, y, x + w / 2, y + h);
+        if (mv) line(x, y + h / 2, x + w, y + h / 2);
+        if (md1) line(x, y, x + w, y + h);
+        if (md2) line(x + w, y, x, y + h);
+      }
+    }
+
+    // draw pen
+    fill(lineColor);
+    noStroke();
+    ellipse(mouseX, mouseY, lineWidth + 2, lineWidth + 2)
+    stroke(0, 50);
+    noFill();
+    ellipse(mouseX, mouseY, lineWidth + 1, lineWidth + 1)
+
+  }
+
 }
 
 function mouseWheel(e) {
-  lineWidth = max(lineWidth += e.delta / 100, 0.1);
+  //lineWidth = max(lineWidth += e.delta / 100, 0.1);
 }
 
 function keyPressed() {
   if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-  if (keyCode === DELETE || keyCode === BACKSPACE) clear();
+  if (keyCode === DELETE || keyCode === BACKSPACE) img.clear();
 
-  if (key == '1') verticalMirror = !verticalMirror;
-  if (key == '2') horizontalMirror = !horizontalMirror;
-  if (key == '3') diagonalMirror = !diagonalMirror;
+  if (keyCode == RIGHT_ARROW) penCount++;
+  if (keyCode == LEFT_ARROW) penCount = max(1, penCount - 1);
+
+  if (keyCode == UP_ARROW) lineWidth++;
+  if (keyCode == DOWN_ARROW) lineWidth = max(1, lineWidth - 1);
+
+  if (key == '1') mv = !mv;
+  if (key == '2') mh = !mh;
+  if (key == '3') md1 = !md1;
+  if (key == '4') md2 = !md2;
+
+  if (key == '5') lineColor = color(0);
+  if (key == '6') lineColor = color(15, 233, 118);
+  if (key == '7') lineColor = color(245, 95, 80);
+  if (key == '8') lineColor = color(65, 105, 185);
+  if (key == '9') lineColor = color(255, 231, 108);
+  if (key == '0') lineColor = color(255);
+
+  if (key == 'd' || key == 'D') showAxes = !showAxes;
   if (key == 'g' || key == 'G') {
     recording = !recording;
     if (!recording) {
