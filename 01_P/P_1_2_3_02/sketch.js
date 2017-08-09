65fafcf78fe35280b8f4bac1c88a832c2e4bcef1
Niels NTG <NTG999@gmail.com>
2017-08-09 16:06:43 +0200

    P_1_2_3_02: Code style improvements

--- 01_P/P_1_2_3_02/sketch.js
+++ 01_P/P_1_2_3_02/sketch.js
@@ -26,7 +26,7 @@
  * s                   : save png
  * c                   : save color palette
  */
-"use strict";
+'use strict';
 
 var colorCount = 20;
 var hueValues = [];
@@ -36,25 +36,24 @@ var actRandomSeed = 0;
 
 
 function setup() {
-  createCanvas(800,800);
-  colorMode(HSB,360,100,100,100);
+  createCanvas(800, 800);
+  colorMode(HSB, 360, 100, 100, 100);
   noStroke();
 }
 
 function draw() {
   noLoop();
-  background("grey");
+  background(127);
   randomSeed(actRandomSeed);
 
   // ------ colors ------
   // create palette
-  for (var i=0; i<colorCount; i++) {
-    if (i%2 == 0) {
+  for (var i = 0; i < colorCount; i++) {
+    if (i % 2 == 0) {
       hueValues[i] = int(random(130, 220));
       saturationValues[i] = 100;
       brightnessValues[i] = int(random(15, 100));
-    }
-    else {
+    } else {
       hueValues[i] = 195;
       saturationValues[i] = int(random(20, 100));
       brightnessValues[i] = 100;
@@ -65,46 +64,47 @@ function draw() {
   // count tiles
   var counter = 0;
   // row count and row height
-  var rowCount = int(random(5,30));
-  var rowHeight = height/rowCount;
+  var rowCount = int(random(5, 30));
+  var rowHeight = height / rowCount;
 
   // seperate each line in parts
-  for(var i=rowCount; i>=0; i--) {
+  for (var i = rowCount; i >= 0; i--) {
     // how many fragments
-    var partCount = i+1;
+    var partCount = i + 1;
     var parts = [];
 
-    for(var ii=0; ii<partCount; ii++) {
+    for (var ii = 0; ii < partCount; ii++) {
       // sub fragments or not?
-      if (random(1) < 0.075) {
+      if (random() < 0.075) {
         // take care of big values
-        var fragments = int(random(2,20));
+        var fragments = int(random(2, 20));
         partCount = partCount + fragments;
-        for(var iii=0; iii<fragments; iii++) {
+        for (var iii = 0; iii < fragments; iii++) {
           parts.push(random(2));
         }
-      }
-      else {
-        parts.push(random(2,20));
+      } else {
+        parts.push(random(2, 20));
       }
     }
 
     // add all subparts
     var sumPartsTotal = 0;
-    for(var ii=0; ii<partCount; ii++) sumPartsTotal += parts[ii];
+    for (var ii = 0; ii < partCount; ii++) {
+      sumPartsTotal += parts[ii];
+    }
 
     // draw rects
     var sumPartsNow = 0;
-    for(var ii=0; ii<parts.length; ii++) {
+    for (var ii = 0; ii < parts.length; ii++) {
       sumPartsNow += parts[ii];
 
-      var x = map(sumPartsNow, 0,sumPartsTotal, 0,width);
-      var y = rowHeight*i;
-      var w = map(parts[ii], 0,sumPartsTotal, 0,width)*-1;
+      var x = map(sumPartsNow, 0, sumPartsTotal, 0, width);
+      var y = rowHeight * i;
+      var w = -map(parts[ii], 0, sumPartsTotal, 0, width);
       var h = rowHeight;
 
       var index = counter % colorCount;
-      var col = color(hueValues[index],saturationValues[index],brightnessValues[index]);
+      var col = color(hueValues[index], saturationValues[index], brightnessValues[index]);
       fill(col);
       rect(x, y, w, h);
 
@@ -123,8 +123,8 @@ function keyPressed() {
   if (key == 'c' || key == 'C') {
     // -- save an ase file (adobe swatch export) --
     var colors = [];
-    for (var i=0; i<hueValues.length; i++) {
-      colors.push(color(hueValues[i],saturationValues[i],brightnessValues[i]));
+    for (var i = 0; i < hueValues.length; i++) {
+      colors.push(color(hueValues[i], saturationValues[i], brightnessValues[i]));
     }
     writeFile([gd.ase.encode(colors)], gd.timestamp(), 'ase');
   }
