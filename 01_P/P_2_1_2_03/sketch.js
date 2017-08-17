f7d025eacc1b7d69bd7d0ed578351c8078ae97cd
Niels NTG <NTG999@gmail.com>
2017-08-17 21:16:11 +0200

    P_2_1_2_03: Removed snake case variable

--- 01_P/P_2_1_2_03/sketch.js
+++ 01_P/P_2_1_2_03/sketch.js
@@ -32,7 +32,7 @@ var actRandomSeed = 0;
 
 var moduleColor;
 var moduleAlpha = 180;
-var max_distance = 500;
+var maxDistance = 500;
 
 function setup() {
   createCanvas(600, 600);
@@ -51,7 +51,7 @@ function draw() {
   for (var gridY = 0; gridY < width; gridY += 25) {
     for (var gridX = 0; gridX < height; gridX += 25) {
       var diameter = dist(mouseX, mouseY, gridX, gridY);
-      diameter = diameter / max_distance * 40;
+      diameter = diameter / maxDistance * 40;
       push();
       translate(gridX, gridY, diameter * 5);
       rect(0, 0, diameter, diameter);    // also nice: ellipse(...)
