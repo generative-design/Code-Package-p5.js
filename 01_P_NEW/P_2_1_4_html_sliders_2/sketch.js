657fb9bdc055c93c3c09a9b58565b697dcae098f
Joey Lee <joeyklee@users.noreply.github.com>
2017-08-20 12:44:38 -0700

    added comments

--- 01_P_NEW/P_2_1_4_html_sliders_2/sketch.js
+++ 01_P_NEW/P_2_1_4_html_sliders_2/sketch.js
@@ -39,20 +39,22 @@ function mousePressed(){
 function SliderRose(_x, _y){
   this.x1 = _x;
   this.y1 = _y;
+  // collect the sliders in an array
   var mySliders = [];
   var sinAngle = 0;
-
+  // create a counter to index the sliders
   var counter = 0;
-  var skip = 20;
+  // set the slider width
   var roseRadius = random(20, 100);
-
+  // define how many degrees to skip from 360
+  var skip = 20;
+  // create sliders around a circle
   for(var i =0; i < 360; i+=skip){
     var sliderAngle = radians(i);
     var x2 = cos(sliderAngle)*roseRadius;
-    var y2 = sin(sliderAngle)*roseRadius;
-
+    var y2 = sin(sliderAngle)*roseRadius; 
+    // create the slider, position, and rotate
     mySliders[counter] = createSlider(0, 255, 50)
-
     mySliders[counter].position(this.x1 + x2, this.y1 + y2);
     mySliders[counter].style('width', roseRadius +'px')
     mySliders[counter].style('transform', 'rotate('+i+'deg)');
@@ -60,17 +62,17 @@ function SliderRose(_x, _y){
 
   }
 
+  // for each loop through the draw function
+  // update the sliders according to a sin wave
   this.update = function(){
     var offset = 0;
-
     for(var i = 0; i < mySliders.length; i++){
+      // map the value along the sine wave to the slider values
       var x = map(sin(sinAngle + offset), -1, 1, 0, 255);
       mySliders[i].value(x);
       offset += 0.050;
     }
-
     sinAngle += 0.1;
-
   }
 
 }
