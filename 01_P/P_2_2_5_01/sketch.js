00deec03f69fefd334b9bc22a61f3f2624d401e2
Niels NTG <NTG999@gmail.com>
2017-08-21 01:33:48 +0200

    P_2_2_5: Code style improvements + bug fixes

--- 01_P/P_2_2_5_01/sketch.js
+++ 01_P/P_2_2_5_01/sketch.js
@@ -1,4 +1,4 @@
-// P_2_2_5_01.pde
+// P_2_2_5_01
 //
 // Generative Gestaltung, ISBN: 978-3-87439-759-9
 // First Edition, Hermann Schmidt, Mainz, 2009
@@ -23,7 +23,10 @@
  * press + position x/y : move area of interest
  *
  * KEYS
+ * 1                    : show/hide circles
+ * 2                    : show/hide lines
  * arrow up/down        : resize area of interest
+ * f                    : freeze process. on/off
  * s                    : save png
  */
 'use strict';
@@ -36,8 +39,13 @@ var maxRadius = 50;
 // for mouse and up/down-arrow interaction
 var mouseRect = 15;
 
+var freeze = false;
+
+var showCircle = true;
+var showLine = true;
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
   noFill();
   cursor(CROSS);
   ellipseMode(RADIUS);
@@ -50,48 +58,45 @@ function draw() {
   // Choose a random or the current mouse position
   var newX = random(maxRadius, width - maxRadius);
   var newY = random(maxRadius, height - maxRadius);
-  var newR = minRadius;
   if (mouseIsPressed && mouseButton == LEFT) {
     newX = random(mouseX - mouseRect, mouseX + mouseRect);
     newY = random(mouseY - mouseRect, mouseY + mouseRect);
-    newR = 1;
   }
 
-  // Check if the small new circle already intersects one of the others
+  // Try to fit the largest possible circle at the current location without overlapping
   var intersection = false;
-  for (var i = 0; i < circles.length; i++) {
-    var d = dist(newX, newY, circles[i].x, circles[i].y);
-    if (d < circles[i].r + newR) {
-      intersection = true;
-    }
-  }
-
-  // If not, try to fit the largest possible circle at the current location without overlapping
-  if (!intersection) {
-    var minR = Number.MAX_VALUE;
-    var closestCircle;
+  for (var newR = maxRadius; newR >= minRadius; newR--) {
     for (var i = 0; i < circles.length; i++) {
-      var d = dist(newX, newY, circles[i].x, circles[i].y);
-      if (minR > d - circles[i].r) {
-        minR = d - circles[i].r;
-        closestCircle = circles[i];
+      intersection = dist(newX, newY, circles[i].x, circles[i].y) < circles[i].r + newR;
+      if (intersection) {
+        break;
       }
     }
-    if (minR > maxRadius) minR = maxRadius;
-    
-    circles.push(new Circle(newX, newY, minR, closestCircle));
+    if (!intersection) {
+      circles.push(new Circle(newX, newY, newR));
+      break;
+    }
   }
 
-  // Draw all circles
   for (var i = 0; i < circles.length; i++) {
-    // Draw the circle itself
-    stroke(0);
-    strokeWeight(1.5);
-    circles[i].draw();
-    // Draw the line between this and the closest
-    stroke(51, 204, 51);
-    strokeWeight(0.75);
-    circles[i].drawLine();
+    if (showLine) {
+      // Try to find an adjacent circle to the current one and draw a connecting line between the two
+      var closestCircle;
+      for (var j = 0; j < circles.length; j++) {
+        if (dist(circles[i].x, circles[i].y, circles[j].x, circles[j].y) <= circles[i].r + circles[j].r + 1) {
+          closestCircle = circles[j];
+          break;
+        }
+      }
+      if (closestCircle) {
+        stroke(100, 230, 100);
+        strokeWeight(0.75);
+        line(circles[i].x, circles[i].y, closestCircle.x, closestCircle.y);
+      }
+    }
+
+    // Draw the circle itself.
+    if (showCircle) circles[i].draw();
   }
 
   // Visualise the random range of the current mouse position
@@ -102,26 +107,35 @@ function draw() {
   }
 }
 
-function Circle(x, y, r, c) {
+function Circle(x, y, r) {
   this.x = x;
   this.y = y;
   this.r = r;
-  this.closestCircle = c;
 
   Circle.prototype.draw = function() {
+    stroke(0);
+    strokeWeight(1.5);
     ellipse(this.x, this.y, this.r);
-  }
-
-  Circle.prototype.drawLine = function() {
-    if (this.closestCircle) line(this.x, this.y, this.closestCircle.x, this.closestCircle.y);
   }
 }
 
 function keyPressed() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+
   if (keyCode == UP_ARROW) mouseRect += 4;
   if (keyCode == DOWN_ARROW) mouseRect -= 4;
-}
 
-function keyReleased() {
-  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  // toggle freeze drawing
+  if (key == 'f' || key == 'F') {
+    freeze = !freeze;
+    if (freeze) {
+      noLoop();
+    } else {
+      loop();
+    }
+  }
+
+  // toggle style
+  if (key == '1') showCircle = !showCircle;
+  if (key == '2') showLine = !showLine;
 }
